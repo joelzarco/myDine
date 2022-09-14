63c1bb35b91ff7e41bc7de88b74c9f1db577d357
joelzarco <joelzarco@me.com>
2022-09-14 18:35:30 -0500

    List added and populated using decoded json

---
+++ iDine/iDine/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Johel Zarco on 14/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // decode menu.json with helper
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    var body: some View {
+        
+        NavigationView{
+            List{
+                ForEach(menu){ section in // as in closure
+                    // SwitUI already has a section/header functionality ready to use
+                    Section(header : Text(section.name)){
+                        ForEach(section.items){ item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Menu")// applied to List
+            // .listStyle() no longer available to List()
+        }
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Johel Zarco on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
